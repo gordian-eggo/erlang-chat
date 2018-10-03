891494d329e9d4d113dfb70d9f5de72b37b33565
gordian-eggo <columbus.knot@gmail.com>
2018-10-04 06:37:07 +0800

    Need more documentation to debug, functions entering infinite loop.

--- chat.erl
+++ chat.erl
@@ -1,22 +1,26 @@
 -module(chat).
 -compile(export_all).
 
-start1()->
+% use -sname when running para kumabit yung net_adm:ping()
+
+start1()->        % run in first terminal
   register(user1, spawn(chat, user1, [])).
 
-user1()->
+user1()->         % run in first terminal
+  % Debug = "debug string~n",
+  % Debug,
   receive
-    {User_Pid, input}->
+    {User_Pid, Input}->
       Input = io:get_line("Input: "),
       User_Pid ! {self(), input};
     {User_Pid, Message}->
       io:format(Message),
       Input = io:get_line("Input: "),
-      User_Pid ! input
+      User_Pid ! Input
   end,
-  user1() ! input.
+  user1() ! Input.
 
-start2(User_Pid)->
+start2(User_Pid)->          % start second chat member
   spawn(chat, user2, [1, User_Pid]).
 
 user2(0, User_Pid)->
